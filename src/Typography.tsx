10246bc0dd5c312f73e3a28f5a220d10326ae064
Charles Mutagorama <c.mutagorama@irembo.com>
2021-12-22 20:42:23 +0200

    implemented typography component

---
+++ src/Typography.tsx
@@ -0,0 +1,88 @@
+import React, { FC } from "react";
+import classNames from "classnames";
+
+type TypographyVariant =
+  // Text
+  | "xs"
+  | "sm"
+  | "md"
+  | "lg"
+  | "xl"
+  // Display
+  | "h6"
+  | "h5"
+  | "h4"
+  | "h3"
+  | "h2"
+  | "h1";
+
+type TypographyWeightOption = "regular" | "medium" | "semibold" | "bold";
+
+type TypographyWeightValue =
+  | "font-normal"
+  | "font-medium"
+  | "font-semibold"
+  | "font-bold";
+
+export interface TypographyProps {
+  variant: TypographyVariant;
+  customColor?: string;
+  customWeight?: TypographyWeightOption;
+  className?: string;
+  children: string | React.ReactNode;
+}
+
+const TypograhyVariantClasses: Record<TypographyVariant, string> = {
+  xs: "text-xs",
+  sm: "text-sm",
+  md: "text-md",
+  lg: "text-lg",
+  xl: "text-xl",
+  h6: "text-h6",
+  h5: "text-h5",
+  h4: "text-h4",
+  h3: "text-h3",
+  h2: "text-h2",
+  h1: "text-h1",
+};
+
+const TypographyWeightClasses: Record<
+  TypographyWeightOption,
+  TypographyWeightValue
+> = {
+  regular: "font-normal",
+  medium: "font-medium",
+  semibold: "font-semibold",
+  bold: "font-bold",
+};
+
+export const Typography: FC<TypographyProps> = ({
+  variant = "md",
+  customColor,
+  customWeight = "regular",
+  className,
+  children,
+}) => {
+  const TypographyVariantClassName = TypograhyVariantClasses[variant];
+  const TypographyWeightClassName = TypographyWeightClasses[customWeight];
+
+  const isHeading = variant.startsWith("h");
+  const Component = (isHeading ? variant : "p") as keyof JSX.IntrinsicElements;
+
+  return (
+    <Component
+      className={classNames(
+        TypographyVariantClassName,
+        TypographyWeightClassName,
+        className,
+        {
+          ["tracking-tight"]: ["h1", "h2", "h3"].includes(variant),
+          ["text-gray-900 dark:text-white"]: !customColor,
+        },
+        customColor,
+      )}
+    >
+      {children}
+    </Component>
+  );
+};
