f9d41c3d07002572383b9bca7455e71d2e0fde66
Charles Mutagorama <c.mutagorama@irembo.com>
2021-12-23 23:35:45 +0200

    docs(buttongroup): added figma link of the button group component

--- src/data/figma.tsx
+++ src/data/figma.tsx
@@ -1,8 +1,10 @@
 type Component =
 | "Colors"
-| "Typography";
+| "Typography"
+| "ButtonGroup";
 
 export const Figma: Record<Component, string> = {
     Colors: "https://www.figma.com/file/TMBfRlQjnb3ko7MRBrtpw1/?node-id=5%3A15",
-    Typography: "https://www.figma.com/file/TMBfRlQjnb3ko7MRBrtpw1/?node-id=6%3A25187"
+    Typography: "https://www.figma.com/file/TMBfRlQjnb3ko7MRBrtpw1/?node-id=6%3A25187",
+    ButtonGroup: "https://www.figma.com/file/TMBfRlQjnb3ko7MRBrtpw1/?node-id=11%3A14242"
 }
