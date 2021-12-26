e439de5e09436deb14bd42701dc0040383b12f5c
Charles Mutagorama <c.mutagorama@irembo.com>
2021-12-26 21:44:08 +0200

    docs(figma.tsx): added figma link

--- src/data/figma.tsx
+++ src/data/figma.tsx
@@ -1,10 +1,12 @@
 type Component =
 | "Colors"
 | "Typography"
-| "ButtonGroup";
+| "ButtonGroup"
+| "Pagination";
 
 export const Figma: Record<Component, string> = {
     Colors: "https://www.figma.com/file/TMBfRlQjnb3ko7MRBrtpw1/?node-id=5%3A15",
     Typography: "https://www.figma.com/file/TMBfRlQjnb3ko7MRBrtpw1/?node-id=6%3A25187",
-    ButtonGroup: "https://www.figma.com/file/TMBfRlQjnb3ko7MRBrtpw1/?node-id=11%3A14242"
+    ButtonGroup: "https://www.figma.com/file/TMBfRlQjnb3ko7MRBrtpw1/?node-id=11%3A14242",
+    Pagination: "https://www.figma.com/file/TMBfRlQjnb3ko7MRBrtpw1/?node-id=13%3A24715"
 }
