f35e952c94534b16d5a0326fc011c4a365fbec3a
Charles Mutagorama <charles.mutagorama@risa.gov>
2022-02-24 10:44:25 +0200

    feat(index.tsx): exported badge component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
+// components
 export * from "./ColorBox";
 export * from "./Typography";
 export * from "./ButtonGroup";
 export * from "./Pagination";
+export * from "./Badge";
+
+// assets
+export * from "./data/images";
