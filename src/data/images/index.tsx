51a5289f088a2bd04f170c4477e7703c6f5d54f5
Charles Mutagorama <charles.mutagorama@risa.gov>
2022-02-24 16:19:08 +0200

    fix(images): accessing images deployed on the cloud

--- src/data/images/index.tsx
+++ src/data/images/index.tsx
@@ -2,13 +2,22 @@ export type CountryISO = "BI" | "CD" | "KE" | "RW" | "TZ" | "UG";
 
 type image = CountryISO;
 
-const devImages: Record<image, string> = {
-	BI: require("./bi.svg"),
-	CD: require("./cd.svg"),
-	KE: require("./ke.svg"),
-	RW: require("./rw.svg"),
-	TZ: require("./tz.svg"),
-	UG: require("./ug.svg")
-};
+// const devImages: Record<image, string> = {
+// 	BI: require("./bi.svg"),
+// 	CD: require("./cd.svg"),
+// 	KE: require("./ke.svg"),
+// 	RW: require("./rw.svg"),
+// 	TZ: require("./tz.svg"),
+// 	UG: require("./ug.svg")
+// };
 
-export const images = devImages;
+const prodImages: Record<image, string> = {
+	BI: 'https://storage.googleapis.com/umucyo-ui/bi.svg',
+	CD: 'https://storage.googleapis.com/umucyo-ui/cd.svg',
+	KE: 'https://storage.googleapis.com/umucyo-ui/ke.svg',
+	RW: 'https://storage.googleapis.com/umucyo-ui/rw.svg',
+	TZ: 'https://storage.googleapis.com/umucyo-ui/tz.svg',
+	UG: 'https://storage.googleapis.com/umucyo-ui/ug.svg'
+}
+
+export const images = prodImages;
